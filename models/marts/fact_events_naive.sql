f58a7b4dfda567c74edafe6f1069aea018c7b7ae
Retugbo <70543473+Retugbo@users.noreply.github.com>
2025-12-19 01:08:28 +0000

    Update fact_events_naive.sql

--- models/marts/fact_events_naive.sql
+++ models/marts/fact_events_naive.sql
@@ -1,1 +1,23 @@
 
+{{ config(materialized='table') }}
+
+with revenue_adjustments as (
+    select
+        entity_key,
+        period_key,
+        adjustment_category,
+        sum(adjustment_amount) as adjustment_amount
+    from {{ ref('stg_revenue_adjustments') }}
+    group by entity_key, period_key, adjustment_category
+)
+
+select
+    e.event_id,
+    e.event_business_key,
+    e.entity_key,
+    sum(coalesce(r.adjustment_amount,0)) as non_core_amount
+from {{ ref('stg_events') }} e
+left join revenue_adjustments r
+    on r.entity_key = e.entity_key
+    and r.period_key = e.period_key
+group by e.event_id, e.event_business_key, e.entity_key

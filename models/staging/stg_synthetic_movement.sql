5173c810b6ca50bc79e475d6bc00299e4ff870a9
Retugbo <70543473+Retugbo@users.noreply.github.com>
2025-12-20 15:11:35 +0000

    Add staging model for synthetic movement data

--- models/staging/stg_synthetic_movement.sql
+++ models/staging/stg_synthetic_movement.sql
@@ -1,1 +1,20 @@
+{{ config(materialized='view') }}
+
+-- Staging model built on synthetic data
+-- Mimics movement-level operational records
+-- Used to demonstrate data skew and optimisation patterns
+
+select
+    location_id,
+    activity_date,
+    revenue_amount
+from {{ ref('movement_non_skewed') }}
+
+union all
+
+select
+    location_id,
+    activity_date,
+    revenue_amount
+from {{ ref('movement_skewed') }}
 

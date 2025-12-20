0515870d4668559f2f5e33be91506711eb223251
Retugbo <70543473+Retugbo@users.noreply.github.com>
2025-12-20 16:29:29 +0000

    Add final optimised revenue model combining skewed paths

--- models/marts/revenue/revenue_optimized.sql
+++ models/marts/revenue/revenue_optimized.sql
@@ -1,1 +1,11 @@
+{{ config(materialized='table') }}
+
+-- Final optimised revenue model
+-- Combines skewed and non-skewed execution paths
+
+select * from {{ ref('revenue_skewed') }}
+
+union all
+
+select * from {{ ref('revenue_non_skewed') }}
 
